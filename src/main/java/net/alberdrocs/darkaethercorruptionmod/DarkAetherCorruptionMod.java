3c06f58ae68dd92113e565a872da28ec5bf4c0a6
Alberto Gonzalez Abril <alberto@gonzabril.es>
2024-01-18 11:57:24 +0000

    Added Dark Aether and Refined Dark Aether Crystal items

--- src/main/java/net/alberdrocs/darkaethercorruptionmod/DarkAetherCorruptionMod.java
+++ src/main/java/net/alberdrocs/darkaethercorruptionmod/DarkAetherCorruptionMod.java
@@ -1,6 +1,9 @@
 package net.alberdrocs.darkaethercorruptionmod;
 
 import com.mojang.logging.LogUtils;
+import net.alberdrocs.darkaethercorruptionmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +30,8 @@ public class DarkAetherCorruptionMod
     public DarkAetherCorruptionMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -44,6 +49,10 @@ public class DarkAetherCorruptionMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.DARK_AETHER_CRYSTAL);
+            event.accept(ModItems.REFINED_DARK_AETHER_CRYSTAL);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/alberdrocs/darkaethercorruptionmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.alberdrocs.darkaethercorruptionmod.item;
+
+import net.alberdrocs.darkaethercorruptionmod.DarkAetherCorruptionMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, DarkAetherCorruptionMod.MOD_ID);
+
+    public static final RegistryObject<Item> DARK_AETHER_CRYSTAL =
+            ITEMS.register("darkaethercrystal", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> REFINED_DARK_AETHER_CRYSTAL =
+            ITEMS.register("refineddarkaethercrystal", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
