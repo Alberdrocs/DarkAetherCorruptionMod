749c85ea12b19e9bc85dec5a1e398452e5a3335a
Alberto Gonzalez Abril <alberto@gonzabril.es>
2024-03-07 12:13:03 +0000

    Added Experimentation Facilities generation

--- src/main/java/net/alberdrocs/darkaethercorruptionmod/block/custom/AetherNeutralizerBlock.java
+++ src/main/java/net/alberdrocs/darkaethercorruptionmod/block/custom/AetherNeutralizerBlock.java
@@ -1,17 +1,28 @@
 package net.alberdrocs.darkaethercorruptionmod.block.custom;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class AetherNeutralizerBlock extends Block {
     public AetherNeutralizerBlock(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        System.out.println("Aether Neutralizer broken");
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 
     @Override
